59de4e827ceb8910970c920106021508792ccfe7
nazilaeshghi <nazila.eshghi@gmail.com>
2023-06-14 12:04:07 +0330

    Initial Commit

---
+++ from rep/from_repApp.swift
@@ -0,0 +1,17 @@
+//
+//  from_repApp.swift
+//  from rep
+//
+//  Created by Nazila Eshghi on 3/24/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct from_repApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
