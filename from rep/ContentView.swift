fe4b1f53838a421377605b012a50ed89e7a3c3f3
nazilaeshghi <nazila.eshghi@gmail.com>
2023-06-14 12:49:31 +0330

    fixed bug b

--- from rep/ContentView.swift
+++ from rep/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world again bug a!")
+            Text("Hello, world again bug b!")
         }
         .padding()
     }
